ad84316c88fae36f2ac7a6e8c82b7c8542094465
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-11-27 01:06:38 +0530

    Practice 03

---
+++ Practice03.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\n");
+    // Exercise 01
+    printf("Hello World!");
+    printf("This is our C course!");
+    // Output: Hello World!This is our C course!
+    printf("\n\n");
+
+    // Exercise 02
+    printf("Hello World!\n");
+    printf("This is our C course!\n\n");
+    // Output:
+    // Hello World!
+    // This is our C course!
+
+    // Exercise 03
+    printf("\t\t\t*\n");
+    printf("\t\t*\t*\t*\n");
+    printf("\t*\t*\t*\t*\t*\n");
+    printf("*\t*\t*\t*\t*\t*\t*\n\n");
+
+    // Output:
+
+    //                         *
+    //                 *       *       *
+    //         *       *       *       *       *
+    // *       *       *       *       *       *       *
+
+    // Exercise 04
+    printf("Escape Characters: \" \' \\ \?\n\n");
+    return 0;
+
+    // Single Line Comment
+
+    /*
+    Multiple
+    Line
+    Comment
+    */
+}
