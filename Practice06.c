5161730ce9f61fbf7616f10a1e62a6fede97e0e9
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-11-27 10:08:45 +0530

    Practice 06

---
+++ Practice06.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int x = 0;
+    printf("\n%d", x);
+    printf("\n\nI have six mangoes.\n\n");
+    printf("I have %d mangoes. \n\n", 10);
+    printf("I have 6 %s and 5 %s", "mangoes", "oranges.");
+    printf("\n");
+
+    short age;
+    age = 30;
+    printf("\nMy age is %d.\n", age);
+
+    float pi;
+    pi = 3.141;
+    printf("\nValue of PI is %1.3f.\n", pi);
+}
