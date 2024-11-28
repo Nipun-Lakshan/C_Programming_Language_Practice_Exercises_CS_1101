3637a8688bc7fd1caac39bbd838715cea86f4c9f
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-11-28 08:32:13 +0530

    Practice 11

---
+++ Practice11.c
@@ -0,0 +1,12 @@
+// CS1101 - Day 02 - Exercise 01
+#include <stdio.h>
+int main()
+{
+    printf("\n");
+    printf("Hello World!\n\n");
+    printf("Hello World!\n");
+    printf("Faculty of Science\n");
+    printf("University of Colombo\n");
+    printf("Colombo 03\n");
+    return 0;
+}
---
+++ Practice12.c
@@ -0,0 +1,9 @@
+// CS1101 - Day 02 - Exercise 02
+#include <stdio.h>
+int main()
+{
+    printf("\nColumn 1\t\tColumn 2\t\tColumn 3\n");
+    printf("\n10\t\t\t20\t\t\t30\n");
+    printf("\n40\t\t\t50\t\t\t60\n");
+    return 0;
+}
