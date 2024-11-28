5409516b7425dac46007b9c6ace1a1581eb8a295
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-11-28 09:17:00 +0530

    Practice 18

---
+++ Practice18.c
@@ -0,0 +1,11 @@
+// CS1101 - Day 02 - Exercise 08
+#include <stdio.h>
+int main()
+{
+    short number = 10;
+    char letter = 'a';
+    float pi = 3.14;
+    double value = 1.2345678900;
+    printf("\nNumber = %d, Letter = %c, PI = %1.2f, Value = %1.10lf", number, letter, pi, value);
+    return 0;
+}
