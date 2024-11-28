25b1b3d23fa16dbdb43034d4812e1d9b9453cd14
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-11-28 08:45:29 +0530

    Practice 14

--- Practice14.c
+++ Practice14.c
@@ -1,7 +1,11 @@
-// CS1101 - Day 02 - Exercise 03
+// CS1101 - Day 02 - Exercise 04
 #include <stdio.h>
+// include <stdio.h>
 int main()
 {
-    //
+    // int 1_one = 25; # Cannot start variable name with a number!
+    // printf("%d", 1_one)
+    int one_1 = 25;
+    printf("\n%d\n", one_1);
     return 0;
 }
