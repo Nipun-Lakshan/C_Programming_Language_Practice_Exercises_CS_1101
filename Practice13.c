0e837ac0fe9bf48121dbad4339c0539056b1c235
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-11-28 08:37:04 +0530

    Practice 13

---
+++ Practice13.c
@@ -0,0 +1,9 @@
+// CS1101 - Day 02 - Exercise 03
+#include <stdio.h>
+int main()
+{
+    printf("\n@Username : John_Doe\n");
+    printf("Date      : 2024\\11\\25\n");
+    printf("Message   : \"Don't forget to watch the videos in LMS!\"\n");
+    return 0;
+}
---
+++ Practice14.c
@@ -0,0 +1,7 @@
+// CS1101 - Day 02 - Exercise 03
+#include <stdio.h>
+int main()
+{
+    //
+    return 0;
+}
