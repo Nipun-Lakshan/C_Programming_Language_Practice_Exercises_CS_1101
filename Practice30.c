65c14421cddb146f3cf0afb31d651edf5561764a
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-12-03 21:25:19 +0530

    Practice 30

--- Practice30.c
+++ Practice30.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 int main()
 {
-    //
+    printf("\n");
+    printf("============================\n");
+    printf("Printing Numbers from 1 to 5\n");
+    printf("============================\n");
+    int i = 1;
+    while (i <= 5)
+    {
+        printf("\n%d", i);
+        i++;
+    }
+    printf("\n\n");
+    printf("============================\n");
     return 0;
 }
