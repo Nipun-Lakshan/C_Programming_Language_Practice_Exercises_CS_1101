0415f72d2f03aa3171fc37bc278d4baf1ff85cb7
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-12-04 15:07:09 +0530

    Practice 33

--- Practice33.c
+++ Practice33.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 int main()
 {
-    printf("Enter any positive interger: ");
+    printf("\nEnter any positive integer: ");
+    int number = 0;
+    scanf("%d", &number);
+    if (number > 0)
+    {
+        int temp = number;
+        long long sum = 0;
+        while (number != 0)
+        {
+            sum += number;
+            number--;
+        }
+        printf("The sum of natural numbers up to %d: %lld\n", temp, sum);
+    }
+    else
+    {
+        printf("Wrong Input!");
+    }
     return 0;
 }
