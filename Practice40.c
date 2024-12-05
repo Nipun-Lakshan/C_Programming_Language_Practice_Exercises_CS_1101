3536174f55ecc115ba61a4988d5452c9297b26c7
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-12-05 08:39:18 +0530

    Practice 40

--- Practice40.c
+++ Practice40.c
@@ -1,16 +1,21 @@
 // CS1101 - Day 03 - Exercise 02
+
 #include <stdio.h>
+
 int main()
+
 {
+
     float temperature = 1;
     printf("\n");
     puts("Enter the Temperature in Celsius  : - ");
     scanf("%f", &temperature);
+
     float Fahrenheit = 1;
     Fahrenheit = ((temperature * 9) / 5) + 32;
     printf("Entered Temperature in Fahrenheit : - \n%.1f\n", Fahrenheit);
 
-    printf("\n%.2f C = %.2f F\n", temperature, Fahrenheit);
+    printf("\n%.2f°C = %.2f°F\n", temperature, Fahrenheit);
 
     return 0;
 }
