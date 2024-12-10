2709280505311ea4c199da534e551dfb96a17412
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-12-10 18:33:05 +0530

    Practice 59

--- Practice59.c
+++ Practice59.c
@@ -3,7 +3,9 @@
 #include <stdio.h>
 
 int main()
+
 {
+
     // Declaring two variables
 
     float num1;
@@ -11,6 +13,21 @@ int main()
 
     // Requesting the values for num1 and num2 from user
 
-    printf("Enter value for num1 : ");
+    printf("\nEnter a value for num1 : ");
+    scanf("%f", &num1);
+    printf("Enter a value for num2 : ");
+    scanf("%f", &num2);
+
+    // Swapping the Values of the variables
+
+    num1 = num1 + num2;
+    num2 = num1 - num2;
+    num1 = num1 - num2;
+
+    // Print the Result
+
+    printf("\nSwapped Value for num1 : %.2f", num1);
+    printf("\nSwapped Value for num2 : %.2f\n", num2);
+
     return 0;
 }
