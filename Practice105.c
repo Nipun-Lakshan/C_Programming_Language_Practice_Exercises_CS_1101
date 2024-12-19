5d5bc8af97c3b4ffb1e5ffc23d4562b6acda5ce9
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-12-19 11:31:13 +0530

    Practice 105

---
+++ Practice105.c
@@ -0,0 +1,73 @@
+// CS1101 - Day 05 - Exercise 09
+
+#include <stdio.h>
+
+int main()
+
+{
+
+    // Variable Declaration
+
+    int n, i;
+    int max_position;
+    int max;
+    int temp;
+    int j;
+
+    // Array Declaration
+
+    int myArray[] = {3, 8, 5, 12, 4, 17, 22, 12, 16};
+
+    // Calculate Array Size
+
+    n = sizeof(myArray) / sizeof(myArray[0]);
+
+    // Print the Array
+
+    printf("\n[");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d, ", myArray[i]);
+    }
+    printf("\b\b]\n");
+
+    // Heap Sort
+
+    for (j = n; j > 0; j--)
+
+    {
+
+        // Find the Maximum  Element
+
+        max = myArray[0];
+        max_position = 0;
+
+        for (i = 1; i < j; i++)
+        {
+            if (max < myArray[i])
+            {
+                max = myArray[i];
+                max_position = i;
+            }
+        }
+
+        // Swap the Largest Element to Last Element
+
+        temp = myArray[(j - 1)];
+        myArray[(j - 1)] = myArray[max_position];
+        myArray[max_position] = temp;
+    }
+
+    // Print the Array
+
+    printf("\n[");
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d, ", myArray[i]);
+    }
+    
+    printf("\b\b]\n");
+
+    return 0;
+}
