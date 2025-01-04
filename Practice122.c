65b1cad308b8fa1e35c3c51ba0348c464583c51a
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-04 15:49:18 +0530

    Practice 121

---
+++ Practice122.c
@@ -0,0 +1,22 @@
+// CS1101 - Day 06 - Exercise 05 - LCG Algorithm
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    // Variable Declaration
+    int i, a = 5, b = 13, x = 20, MAX_VAL = 201;
+
+    // LCG Algorithm
+    x = time(NULL); // Current Time in Seconds
+    for (i = 0; i < 10; i++)
+    {
+        x = (a * x + b) % MAX_VAL;
+        printf("\n%d", (x - 100)); // Generate Numbers From -100 to 100.
+        printf("\n");
+    }
+
+    return 0;
+}
