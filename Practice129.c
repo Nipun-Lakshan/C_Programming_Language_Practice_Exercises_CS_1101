c728741292fe35511aa2f0ef068efbc3f1f86544
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-09 08:16:25 +0530

    Practice 129

---
+++ Practice129.c
@@ -0,0 +1,24 @@
+// CS 1101 - Day 07 - Exercise 02
+
+#include <stdio.h>
+
+void displayUser(void); // Function Declaration
+
+int main()
+{
+    // Calling the Function
+    displayUser();
+    return 0;
+}
+
+// Function
+void displayUser(void)
+{
+    printf("\nName    : W.M.M.A. Vikum Bandara");
+    printf("\nEmail   : vikumbandara@gmail.com");
+    printf("\nAddress : No. 23, Maharagama, Nugegoda\n");
+
+    printf("\nName    : Hiran Jayaweera");
+    printf("\nEmail   : hiran@phys.cmb.ac.lk");
+    printf("\nAddress : Department of Phyics - UOC\n");
+}
