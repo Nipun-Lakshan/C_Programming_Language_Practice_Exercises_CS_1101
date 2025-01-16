bf4db6fb8320dd01d000ade60a0cc8907215e85d
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-16 09:13:05 +0530

    Practice 148

--- Practice147.c
+++ Practice147.c
@@ -6,10 +6,16 @@
 /* Main Function */
 int main(void)
 {
+    // Variable Declaration
     int n, i;
+
+    // Array Declaration
     int myArray[] = {3, 8, 5, 12, 4, 17, 22, 12, 16, 25, 19, 10, 9, 6, 1};
+
+    // Array Size Calculation
     n = sizeof(myArray) / sizeof(myArray[0]);
 
+    // Print Array
     printf("\n[");
     for (i = 0; i < n; i++)
     {
--- Practice148.c
+++ Practice148.c
@@ -3,13 +3,35 @@
 /* Libraries */
 #include <stdio.h>
 
+/* Function Declarations */
+void printArray(int myArray[], int n);
+
 /* Main Function */
 int main(void)
 {
+    // Variable Declaration
     int n, i;
+
+    // Array Declaration
     int myArray[] = {3, 8, 5, 12, 4, 17, 22, 12, 16, 25, 19, 10, 9, 6, 1};
+
+    // Array Size Calculation
     n = sizeof(myArray) / sizeof(myArray[0]);
 
+    // Print Array
+    printArray(myArray, n);
+
+    return 0;
+}
+
+/* Functions */
+void printArray(int myArray[], int n)
+{ // n - Array Size
+
+    // Variable Declaration
+    int i; // Loop's Index
+
+    // Print Array
     printf("\n[");
     for (i = 0; i < n; i++)
     {
@@ -23,6 +45,4 @@ int main(void)
             printf("]\n");
         }
     }
-
-    return 0;
 }
